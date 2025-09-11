5824cdf26bc9c941130dec5a672aad7459f1b447
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2025-09-11 11:57:32 -0400

    Add shadcn-svelte to website

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+import { Button } from "$lib/components/ui/button/index.js";
+</script>
+
 <h1>Welcome to Kittynode</h1>
 <a href="https://docs.kittynode.com">Docs</a>
+
+<Button>Click me</Button>
