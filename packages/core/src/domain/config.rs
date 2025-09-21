98bde17bb8bf09a1e88f494f11d9474890741239
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2025-09-21 08:02:42 -0400

    Support legacy snake_case config keys

--- packages/core/src/domain/config.rs
+++ packages/core/src/domain/config.rs
@@ -4,9 +4,10 @@ use serde::{Deserialize, Serialize};
 #[serde(rename_all = "camelCase")]
 pub struct Config {
     pub capabilities: Vec<String>,
+    #[serde(alias = "server_url")]
     pub server_url: String,
-    #[serde(default)]
+    #[serde(default, alias = "onboarding_completed")]
     pub onboarding_completed: bool,
-    #[serde(default)]
+    #[serde(default, alias = "auto_start_docker")]
     pub auto_start_docker: bool,
 }
