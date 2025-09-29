549e126dd71f9de96f3e08f103897d862769c9f2
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2025-09-29 04:43:24 -0400

    Centralize package lookups in infra package module

--- packages/core/src/application/delete_package.rs
+++ packages/core/src/application/delete_package.rs
@@ -1,12 +1,9 @@
-use crate::infra::package::{self, get_packages};
+use crate::infra::package;
 use eyre::Result;
 use tracing::info;
 
 pub async fn delete_package(name: &str, include_images: bool) -> Result<()> {
-    let package = get_packages()?
-        .get(name)
-        .ok_or_else(|| eyre::eyre!("Package '{}' not found", name))?
-        .clone();
+    let package = package::get_package_by_name(name)?;
 
     package::delete_package(&package, include_images).await?;
     info!("Package '{}' deleted successfully.", name);
--- packages/core/src/application/install_package.rs
+++ packages/core/src/application/install_package.rs
@@ -1,17 +1,11 @@
-use crate::infra::{
-    file::generate_jwt_secret,
-    package::{self, get_packages},
-    package_config::PackageConfigStore,
-};
+use crate::infra::{file::generate_jwt_secret, package, package_config::PackageConfigStore};
 use eyre::{Context, Result};
 use tracing::info;
 
 pub async fn install_package(name: &str) -> Result<()> {
     generate_jwt_secret().wrap_err("Failed to generate JWT secret")?;
 
-    let package = get_packages()?
-        .remove(name)
-        .ok_or_else(|| eyre::eyre!("Package '{}' not found", name))?;
+    let package = package::get_package_by_name(name)?;
 
     let config = PackageConfigStore::load(name)?;
     let network = config.values.get("network");
