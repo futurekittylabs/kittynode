a502fc39b4e92f7a0febb77a08fe1c727c5a074c
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2025-10-14 15:33:46 -0400

    Add code component

--- website/src/lib/components/ui/button/button.svelte
+++ website/src/lib/components/ui/button/button.svelte
@@ -72,65 +72,63 @@ export type ButtonProps = AnchorElementProps | ButtonElementProps;
 </script>
 
 <script lang="ts">
-  import { cn } from "$lib/utils.js";
-  import LoaderCircleIcon from "@lucide/svelte/icons/loader-circle";
+	import { cn } from '$lib/utils.js';
+	import LoaderCircleIcon from '@lucide/svelte/icons/loader-circle';
 
-  let {
-    ref = $bindable(null),
-    variant = "default",
-    size = "default",
-    href = undefined,
-    type = "button",
-    loading = false,
-    disabled = false,
-    tabindex = 0,
-    onclick,
-    onClickPromise,
-    class: className,
-    children,
-    ...rest
-  }: ButtonProps = $props();
+	let {
+		ref = $bindable(null),
+		variant = 'default',
+		size = 'default',
+		href = undefined,
+		type = 'button',
+		loading = false,
+		disabled = false,
+		tabindex = 0,
+		onclick,
+		onClickPromise,
+		class: className,
+		children,
+		...rest
+	}: ButtonProps = $props();
 </script>
 
 <!-- This approach to disabled links is inspired by bits-ui see: https://github.com/huntabyte/bits-ui/pull/1055 -->
 <svelte:element
-  this={href ? "a" : "button"}
-  {...rest}
-  data-slot="button"
-  type={href ? undefined : type}
-  href={href && !disabled ? href : undefined}
-  disabled={href ? undefined : disabled || loading}
-  aria-disabled={href ? disabled : undefined}
-  role={href && disabled ? "link" : undefined}
-  tabindex={href && disabled ? -1 : tabindex}
-  class={cn(buttonVariants({ variant, size }), className)}
-  bind:this={ref}
-  onclick={async (
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    e: any,
-  ) => {
-    onclick?.(e);
+	this={href ? 'a' : 'button'}
+	{...rest}
+	data-slot="button"
+	type={href ? undefined : type}
+	href={href && !disabled ? href : undefined}
+	disabled={href ? undefined : disabled || loading}
+	aria-disabled={href ? disabled : undefined}
+	role={href && disabled ? 'link' : undefined}
+	tabindex={href && disabled ? -1 : tabindex}
+	class={cn(buttonVariants({ variant, size }), className)}
+	bind:this={ref}
+	onclick={async (
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		e: any
+	) => {
+		onclick?.(e);
 
-    if (type === undefined) return;
+		if (type === undefined) return;
 
-    if (onClickPromise) {
-      loading = true;
+		if (onClickPromise) {
+			loading = true;
 
-      await onClickPromise(e);
+			await onClickPromise(e);
 
-      loading = false;
-    }
-  }}
+			loading = false;
+		}
+	}}
 >
-  {#if type !== undefined && loading}
-    <div
-      class="absolute flex size-full place-items-center justify-center bg-inherit"
-    >
-      <div class="flex animate-spin place-items-center justify-center">
-        <LoaderCircleIcon class="size-4" />
-      </div>
-    </div>
-    <span class="sr-only">Loading</span>
-  {/if}
-  {@render children?.()}
+	{#if type !== undefined && loading}
+		<div class="absolute flex size-full place-items-center justify-center bg-inherit">
+			<div class="flex animate-spin place-items-center justify-center">
+				<LoaderCircleIcon class="size-4" />
+			</div>
+		</div>
+		<span class="sr-only">Loading</span>
+	{/if}
+	{@render children?.()}
 </svelte:element>
