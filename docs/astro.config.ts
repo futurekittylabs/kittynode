44b2f042cd6dbb7ca8dd648c4282c6357d1be8b7
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2025-10-22 19:56:24 -0400

    Highlight validator guide with new badge

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -95,6 +95,10 @@ export default defineConfig({
             {
               label: "Set up validator",
               slug: "guides/set-up-validator",
+              badge: {
+                text: "New",
+                variant: "accent",
+              },
             },
           ],
         },
