9be73dcb3dab79ee0e1683c912e448e09a182575
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2025-10-27 14:04:12 -0400

    Remove unnecessary semver.d.ts

--- packages/app/src/types/semver.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "semver" {
-  export function clean(version: string, options?: unknown): string | null;
-  export function gt(
-    version: string,
-    other: string,
-    options?: unknown,
-  ): boolean;
-}
